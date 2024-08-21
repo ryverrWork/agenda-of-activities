8788b25bc41c058aa98f52172f14df1f57b915a9
Verry Haryanto <ryverr21@gmail.com>
2024-08-21 22:48:16 +0700

    FEAT: fix datatables responsive

--- resources/views/dashboard/pages/admin-home.blade.php
+++ resources/views/dashboard/pages/admin-home.blade.php
@@ -21,7 +21,7 @@
     <!-- DataTable with Buttons -->
     <div class="card">
         <div class="card-datatable table-responsive pt-0">
-            <table class="datatables-basic table table-bordered">
+            <table class="datatables-basic table table-bordered table-responsive">
                 <thead>
                     <tr>
                         <th>No</th>
